a30663105ddcfe09e08153ab804157cab8685759
Pepepequas <90289482+Pepepequas@users.noreply.github.com>
2024-05-25 13:41:31 -0700

    finished movement

--- test_player.gd
+++ test_player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 300
 @export var gravity = 30
+@export var jump_force = 300
 
 func _physics_process(delta):
 	if !is_on_floor():
@@ -9,6 +10,10 @@ func _physics_process(delta):
 		if velocity.y > 500:
 			velocity.y = 500
 	
+	if Input.is_action_just_pressed("Jump(A)") && is_on_floor():
+		
+			velocity.y = -jump_force
+		
 	var horizontal_direction = Input.get_axis("D-pad(Left)","D-pad(Right)")
 	
 	velocity.x = speed * horizontal_direction
