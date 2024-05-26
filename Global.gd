5e0cef2a0aa525b158ef76db20ccb10c22b0b84f
DavidGuerreroAragonA01285837 <a01285837@tec.mx>
2024-05-25 23:27:34 -0600

    Leaderboard methods and elements

---
+++ Global.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var player_1_name="Jugador 1";
+var player_1_score=10;
+
+var player_2_name="Jugador 2";
+var player_2_score=8;
+
+var player_3_name="Jugador 3";
+var player_3_score=6;
+
+var player_4_name="Jugador 4";
+var player_4_score=4;
+
+var First;
+var Second;
+var Third;
+var Fourth;
+
+var Pivot;
---
+++ VBoxContainer.gd
@@ -0,0 +1,12 @@
+extends VBoxContainer
+
+var Winner=Dummy1;
+var Second=Dummy2;
+var Third=Dummy3;
+var Fourth=Dummy4;
+
+var Primero;
+var Segundo;
+var Tercero;
+var Cuarto;
+
